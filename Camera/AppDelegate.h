9031ee89f30393044e3bbcfea9f97293765173f2
Zeev Vax <zeev_vax@10.0.1.37>
2011-12-13 13:01:40 -0800

    Initial Commit

---
+++ Camera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by Zeev Vax on 12/13/11.
+//  Copyright (c) 2011 Hoppaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Camera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Zeev Vax on 12/13/11.
+//  Copyright (c) 2011 Hoppaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
